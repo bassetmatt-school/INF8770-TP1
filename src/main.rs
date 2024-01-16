a68578ee33294e986950689d6c19d78cd084872d
Matthieu Basset <bassetmatt@gmail.com>
2024-01-16 15:23:04 -0500

    Rust update, also add huge text for test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::{
 	collections::{hash_map, HashMap},
-	fs,
+	time::Instant,
 };
 
 fn init_dict(msg: &str) -> HashMap<String, String> {
 	let mut dict = HashMap::new();
 	let mut n_symb = 0;
-	for c in msg.chars() {
-		let c = c.to_string();
+	for i in 0..msg.len() {
+		let c = msg[i..i + 1].to_string();
 		// If the character is not in the dictionary, we add it
 		if let hash_map::Entry::Vacant(e) = dict.entry(c) {
-			let bin = format!("{n_symb:b}");
-			e.insert(bin.clone());
+			e.insert(format!("{n_symb:b}"));
 			n_symb += 1;
 		}
 	}
@@ -19,8 +18,7 @@ fn init_dict(msg: &str) -> HashMap<String, String> {
 }
 
 fn update_dict_size(dict: &mut HashMap<String, String>) {
-	let size = dict.len();
-	let size_log = log_size(size);
+	let size_log = log_size(dict.len());
 	dict.iter_mut().for_each(|(_, v)| {
 		let v_int = v.parse::<usize>().unwrap();
 		*v = format!("{v_int:0width$}", width = size_log);
@@ -32,24 +30,24 @@ fn compress(msg: &str, dict: &mut HashMap<String, String>) -> (String, usize) {
 	let mut n_symb = dict.len();
 	let mut i = 0;
 	let mut length = 0;
-	let chars: Vec<char> = msg.chars().collect();
-	while i < chars.len() {
-		let mut next = chars[i].to_string();
-		let mut next_extra = chars[i].to_string();
-		while dict.contains_key(next_extra.as_str()) && i < chars.len() {
+	while i < msg.len() {
+		let init_i = i;
+		let mut next = &msg[i..i + 1];
+		let mut next_extra = &msg[i..i + 1];
+		while dict.contains_key(next_extra) && i < msg.len() {
 			i += 1;
-			next = next_extra.clone();
-			if i < chars.len() {
-				next_extra.push(chars[i]);
+			next = next_extra;
+			if i < msg.len() {
+				next_extra = &msg[init_i..i + 1];
 			}
 		}
-		let bin_code = dict.get(next.as_str()).unwrap().clone();
+		let bin_code = dict.get(next).unwrap().clone();
 		length += bin_code.len();
 		compressed.push_str(&bin_code);
 
-		if i < chars.len() {
+		if i < msg.len() {
 			let bin = format!("{n_symb:b}");
-			dict.insert(next_extra, bin);
+			dict.insert(next_extra.to_string(), bin);
 		}
 		if log_size(n_symb) >= bin_code.len() {
 			update_dict_size(dict);
@@ -64,25 +62,51 @@ fn log_size(x: usize) -> usize {
 	(x as f64).log2().ceil() as usize
 }
 
-fn main() {
-	const FILES: [&str; 5] = [
-		"data/textes/texte_1.txt",
-		"data/textes/texte_2.txt",
-		"data/textes/texte_3.txt",
-		"data/textes/texte_4.txt",
-		"data/textes/texte_5.txt",
-	];
-	let msg = fs::read_to_string(FILES[0]).unwrap();
-	let mut dict = init_dict(&msg);
-	update_dict_size(&mut dict);
-	println!("{:?}", dict);
-	let (compressed, length) = compress(&msg, &mut dict);
-	println!("Compressed: {}", compressed);
-	println!("Length: {}", length);
-	println!("Dict {:?}", dict);
+#[allow(dead_code)]
+fn print_dict(dict: &HashMap<String, String>) {
 	let mut keys = dict.keys().collect::<Vec<&String>>();
 	keys.sort_by_key(|k| i32::from_str_radix(dict.get(*k).unwrap(), 2).unwrap());
 	for k in keys {
 		println!("{}: {}", k, dict.get(k).unwrap());
 	}
+}
+
+fn main() {
+	let start_time = Instant::now();
+	const MESSAGES: [&str; 6] = [
+		include_str!("../data/textes/texte_1.txt"),
+		include_str!("../data/textes/texte_2.txt"),
+		include_str!("../data/textes/texte_3.txt"),
+		include_str!("../data/textes/texte_4.txt"),
+		include_str!("../data/textes/texte_5.txt"),
+		include_str!("../data/textes/texte_6.txt"),
+	];
+	let msg = MESSAGES[5];
+	let mut dict = init_dict(msg);
+	let init_length = msg.len() * (log_size(dict.len()) as usize);
+	let loading_time = start_time.elapsed();
+	update_dict_size(&mut dict);
+
+	// println!("Dict: {:?}", dict);
+	let start_compress = Instant::now();
+	let (_compressed, length) = compress(msg, &mut dict);
+	let compress_time = start_compress.elapsed();
+	let total_time = start_time.elapsed();
+	// println!("Compressed: {}", _compressed);
+	println!("Length: {}", length);
+	println!(
+		"Compression ratio: {:.2}%",
+		100. - (length as f64) / (init_length as f64) * 100.
+	);
+	println!(
+		"Compression factor: {:.2}",
+		(init_length as f64) / (length as f64)
+	);
+	println!("Load time: {:.2}ms", loading_time.as_secs_f32() * 1000.);
+	println!(
+		"Compress time: {:.2}ms",
+		compress_time.as_secs_f32() * 1000.
+	);
+	println!("Total time: {:.2}ms", total_time.as_secs_f32() * 1000.);
+	// print_dict(&dict);
 }
