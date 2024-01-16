742ccdedbc80fa803f8c2e135cdf981baeb4e527
Matthieu Basset <bassetmatt@gmail.com>
2024-01-16 14:19:51 -0500

    Rust ZLW working

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,43 @@ fn update_dict_size(dict: &mut HashMap<String, String>) {
 	});
 }
 
+fn compress(msg: &str, dict: &mut HashMap<String, String>) -> (String, usize) {
+	let mut compressed = String::new();
+	let mut n_symb = dict.len();
+	let mut i = 0;
+	let mut length = 0;
+	let chars: Vec<char> = msg.chars().collect();
+	while i < chars.len() {
+		let mut next = chars[i].to_string();
+		let mut next_extra = chars[i].to_string();
+		while dict.contains_key(next_extra.as_str()) && i < chars.len() {
+			i += 1;
+			next = next_extra.clone();
+			if i < chars.len() {
+				next_extra.push(chars[i]);
+			}
+		}
+		let bin_code = dict.get(next.as_str()).unwrap().clone();
+		length += bin_code.len();
+		compressed.push_str(&bin_code);
+
+		if i < chars.len() {
+			let bin = format!("{n_symb:b}");
+			dict.insert(next_extra, bin);
+		}
+		if log_size(n_symb) >= bin_code.len() {
+			update_dict_size(dict);
+		}
+		n_symb = dict.len();
+		// compressed.push_str(&String::from("'"));
+	}
+	(compressed, length)
+}
+
 fn log_size(x: usize) -> usize {
 	(x as f64).log2().ceil() as usize
 }
+
 fn main() {
 	const FILES: [&str; 5] = [
 		"data/textes/texte_1.txt",
@@ -38,8 +72,17 @@ fn main() {
 		"data/textes/texte_4.txt",
 		"data/textes/texte_5.txt",
 	];
-	let msg = fs::read_to_string(FILES[4]).unwrap();
+	let msg = fs::read_to_string(FILES[0]).unwrap();
 	let mut dict = init_dict(&msg);
 	update_dict_size(&mut dict);
 	println!("{:?}", dict);
+	let (compressed, length) = compress(&msg, &mut dict);
+	println!("Compressed: {}", compressed);
+	println!("Length: {}", length);
+	println!("Dict {:?}", dict);
+	let mut keys = dict.keys().collect::<Vec<&String>>();
+	keys.sort_by_key(|k| i32::from_str_radix(dict.get(*k).unwrap(), 2).unwrap());
+	for k in keys {
+		println!("{}: {}", k, dict.get(k).unwrap());
+	}
 }
